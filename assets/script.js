5e4117f52bdccf0457393f4bdc63adb3ee34dd8a
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-06-07 01:01:38 -0400

    added prompt for password length, and confirms for uppercase, lowercase, numbers, and special characters

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables for storing input
+var passwordLength;
+var uppercase;
+var lowercase;
+var numeric;
+var special;
+
+// Variable for validation
+var validInput;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +18,31 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Generates password
+function generatePassword(){
+  //password length (8-128)
+  passwordLength = prompt("Password length:", "Between 8-128 characters");
+  console.log(passwordLength)
+
+  uppercase = confirm("Include uppercase?");
+  console.log(uppercase)
+
+  lowercase = confirm("Include lowercase?");
+  console.log(lowercase)
+
+  numeric = confirm("Include numbers?");
+  console.log(numeric)
+
+  special = confirm("Include special characters?")
+  console.log(special)
+
+  //character types (yes/no for [lowercase, uppercase, numeric, and/or special characters])
+  //input should be validated and at least one selected
+
+
+  //generate password based on selected criteria
 }
 
 // Add event listener to generate button
